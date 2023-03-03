8e9e9ab1d6c95efc573364761fcbed0b67533115
jotafuentes <joseluisfuentes17@gmail.com>
2023-03-03 17:21:10 +0100

    prueba de git

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -16,7 +16,7 @@ export class Track extends React.Component {
             <div className="Track">
                 <div className="Track-information">
                     <h3>{this.props.track.name}</h3>
-                    <p>{this.props.track.artist} | {this.props.track.album}</p>
+                    <p>{this.props.track.artist} Hola | {this.props.track.album}</p>
                 </div>
                 {this.renderAction()}
             </div>
