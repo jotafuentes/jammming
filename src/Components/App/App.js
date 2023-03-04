badcda6ffbae78e92353f7eb10b81a5dc92ab956
jotafuentes <joseluisfuentes17@gmail.com>
2023-03-04 19:12:17 +0100

    pass down Plyalist to Tracklist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,10 +8,13 @@ import React from 'react';
 export class App extends React.Component {
   constructor(props) {
     super(props)
-      this.state = {searchResults: [{name: "name1" , artist: "artist2", album: 'album1', id: 'id1'}, 
-      {name: "name2" , artist: "artist2", album: 'album2', id: 'id2'},
-      {name: "name3" , artist: "artist3", album: 'album3', id: 'id3'}]
-      }
+    this.state = {
+      searchResults: [{ name: "name1", artist: "artist2", album: 'album1', id: 'id1' },
+      { name: "name2", artist: "artist2", album: 'album2', id: 'id2' },
+      { name: "name3", artist: "artist3", album: 'album3', id: 'id3' }],
+      playlistName: "My playlist",
+      playlistTracks: [{ name: 'playlist1', artist: 'artist1', album: 'album1', id: 'id1' }]
+    }
   }
 
 
@@ -23,14 +26,14 @@ export class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
     );
   }
-  
+
 }
 
 
